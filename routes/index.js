cb65e0a149ef943a7740fb0a19f61d126b4ba292
palmebjoerk <palmenjr@hotmail.com>
2026-05-07 10:41:11 +0200

    views + routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET basket page. */
+router.get('/basket', function(req, res, next) {
+  res.render('basket', { title: 'Basket' });
+});
+
+/* GET favorites page. */
+router.get('/favorites', function(req, res, next) {
+  res.render('favorites', { title: 'Favorites' });
+});
+
+/* GET error page. */
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: 'Error' });
+});
+
+/* GET index page. */
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Index' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* GET news page. */
+router.get('/news', function(req, res, next) {
+  res.render('news', { title: 'News' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+/* GET search page. */
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Search' });
+});
+
+/* GET Admin page. */
+router.get('/admin', function(req, res, next) {
+  res.render('admin/index', { title: 'Admin' });
 });
 
 module.exports = router;
