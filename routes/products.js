0f5ab858dd2c626fd61749f0c5c8fea4379bc1fc
palmebjoerk <palmenjr@hotmail.com>
2026-05-19 11:29:51 +0200

    Uppdaterat SQL, lagt in routes

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/Product");
+
+router.get("/", async (req, res) => {
+
+    const products = await Product.find().limit(8);
+
+    res.render("products", {
+        products
+    });
+});
